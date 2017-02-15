c8b3a7328af7b45b108ea0a04290c05fc9f24b9a
Peeyush.Anand <Peeyush.Anand@IVDB-09>
2017-02-15 11:07:23 +0530

    Add project files.

---
+++ MvcLoginAppDemo/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcLoginAppDemo.Models;
+
+namespace MvcLoginAppDemo.Controllers
+{
+    [RoutePrefix("admin")]
+    
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(loginModel objUser)
+        {
+            DB_Entities db = new DB_Entities();
+            if (ModelState.IsValid)
+            {
+ 
+        
+                    var obj = db.tbl_AdminLogin.Any(a => a.UserName == objUser.UsernName && a.PassWord == objUser.Password);
+                    if (obj)
+                    {
+
+                        Session["UserName"] = (from user in db.tbl_AdminLogin where user.UserName == objUser.UsernName && user.PassWord==objUser.Password
+                                               select user.FullName).First();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Error", "UserName or Password is incorrect");
+                    }
+ 
+            }
+            return View(objUser);
+        }
+        [HttpGet]
+       
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserName"] != null)
+            {
+                ProductDetailModel prod = new ProductDetailModel();
+                prod.listofLogin = ProductData();
+                return View(prod);
+            }
+            else
+            {
+                return RedirectToAction("Login","Home");
+            }
+
+        }
+        public List<loginModel> ProductData()
+        {
+            List<loginModel> objProduct = new List<loginModel>();
+            DB_Entities objDemoEntities = new DB_Entities();
+            var ProductItem = from data in objDemoEntities.tbl_AdminLogin select data;
+            foreach (var item in ProductItem)
+            {
+                    objProduct.Add(new loginModel {
+                    UsernName = item.UserName,
+                    Password = item.PassWord
+                });
+            }
+            return objProduct;
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Login", "Home");
+        }
+
+    }
+}
---
+++ MvcLoginAppDemo/Models/loginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcLoginAppDemo.Models
+{
+    public class ProductDetailModel
+    {
+        public List<loginModel> listofLogin = new List<loginModel>();
+    }
+    public class loginModel
+    {
+        [Required]
+        [DisplayName("username")]
+        public string UsernName { get; set; }
+        [Required]
+        [DisplayName("Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+}
